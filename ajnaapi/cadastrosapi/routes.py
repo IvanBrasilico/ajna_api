d7c51d79e3d00da5b9808476705871cd8a3491a1
IvanBrasilico <brasilico.ivan@gmail.com>
2020-07-11 19:09:50 -0300

    API Eventos Xtra endpoints

--- ajnaapi/cadastrosapi/routes.py
+++ ajnaapi/cadastrosapi/routes.py
@@ -1,24 +1,8 @@
-from flask import Blueprint, current_app, jsonify
+from flask import Blueprint, current_app, jsonify, request
 
-from bhadrasana.models.laudo import get_empresas, get_ncm, get_empresa
+from bhadrasana.models.laudo import get_empresas, get_ncm, get_empresa, get_empresas_nome
 
 cadastrosapi = Blueprint('cadastrosapi', __name__)
-
-
-@cadastrosapi.route('/api/empresas/<cnpj>', methods=['GET'])
-def empresas_(cnpj):
-    db_session = current_app.config['db_session']
-    try:
-        empresas = get_empresas(db_session, cnpj)
-        result = [empresa.dump() for empresa in empresas]
-    except ValueError as err:
-        return jsonify(
-            {'msg': str(err)}), 400
-    except Exception as err:  # pragma: no cover
-        current_app.logger.error(err, exc_info=True)
-        return jsonify(
-            {'msg': 'Erro inesperado! Detalhes no log da aplicação. ' + str(err)}), 500
-    return jsonify(result), 404 if len(result) == 0 else 200
 
 
 @cadastrosapi.route('/api/empresa/<cnpj>', methods=['GET'])
@@ -36,6 +20,41 @@ def empresa_(cnpj):
         return jsonify(
             {'msg': 'Erro inesperado! Detalhes no log da aplicação. ' + str(err)}), 500
     return jsonify(empresa.dump()), 200
+
+
+@cadastrosapi.route('/api/empresa_e_filiais/<cnpj>', methods=['GET'])
+def empresa_filiais(cnpj):
+    db_session = current_app.config['db_session']
+    try:
+        empresas = get_empresas(db_session, cnpj)
+        result = [empresa.dump() for empresa in empresas]
+    except ValueError as err:
+        return jsonify(
+            {'msg': str(err)}), 400
+    except Exception as err:  # pragma: no cover
+        current_app.logger.error(err, exc_info=True)
+        return jsonify(
+            {'msg': 'Erro inesperado! Detalhes no log da aplicação. ' + str(err)}), 500
+    return jsonify(result), 404 if len(result) == 0 else 200
+
+
+@cadastrosapi.route('/api/empresas', methods=['GET'])
+def empresas_():
+    db_session = current_app.config['db_session']
+    try:
+        nome = request.args.get('nome')
+        empresas = get_empresas_nome(db_session, nome)
+        result = [empresa.dump() for empresa in empresas]
+        if len(result) == 0:
+            return jsonify({'msg': 'Empresas não encontradas com filtro %s ' % request.args}), 404
+    except ValueError as err:
+        return jsonify(
+            {'msg': str(err)}), 400
+    except Exception as err:  # pragma: no cover
+        current_app.logger.error(err, exc_info=True)
+        return jsonify(
+            {'msg': 'Erro inesperado! Detalhes no log da aplicação. ' + str(err)}), 500
+    return jsonify(result), 404 if len(result) == 0 else 200
 
 
 @cadastrosapi.route('/api/ncm/<codigo>', methods=['GET'])
